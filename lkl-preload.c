51edc26d93886ab9d286cb746272b9a5958b9b4c
Mark Stillwell <mark@stillwell.me>
2015-12-07 09:58:20 +0000

    open not working...why?

--- lkl-preload.c
+++ lkl-preload.c
@@ -19,7 +19,7 @@
 
 #include <dlfcn.h>
 
-void myprintk(const char *str, int len)
+void printk(const char *str, int len)
 {
     int ret __attribute__((unused));
     ret = write(STDOUT_FILENO, str, len);
@@ -31,17 +31,19 @@ char mpoint[32];
 int __libc_start_main(int (*main)(int,char **,char **), int argc, char **argv,
                       void (*init)(void), void (*fini)(void),
                       void (*rtld_fini)(void), void *stack_end) {
-    int (*original_libc_start_main)(int (*)(int,char **,char **), int, char **,
+    int (*orig_libc_start_main)(int (*)(int,char **,char **), int, char **,
                                     void (*)(void), void (*)(void),
                                     void (*)(void), void *);
 
-    int (*original_open)(const char *, int);
+    int (*orig_open)(const char *, int, ...);
     unsigned int disk_id;
     long ret;
 
-    original_open = dlsym(RTLD_NEXT, "open");
+    //fprintf(stderr, "PRELOAD: __libc_start_main\n");
 
-    bs.fd = original_open("./disk.img", O_RDONLY);
+    orig_open = dlsym(RTLD_NEXT, "open");
+
+    bs.fd = orig_open("./disk.img", O_RDONLY);
     if (bs.fd < 0) {
         fprintf(stderr, "can't open fsimg %s: %s\n", "./hello.img",
             strerror(errno));
@@ -57,7 +59,7 @@ int __libc_start_main(int (*main)(int,char **,char **), int argc, char **argv,
     }
     disk_id = ret;
 
-    lkl_host_ops.print = myprintk;
+    lkl_host_ops.print = printk;
     lkl_start_kernel(&lkl_host_ops, 100 * 1024 * 1024, "");
 
     ret = lkl_mount_dev(disk_id, "ext2", LKL_MS_RDONLY, NULL,
@@ -69,26 +71,33 @@ int __libc_start_main(int (*main)(int,char **,char **), int argc, char **argv,
         exit(ret);
     }
 
-    original_libc_start_main = dlsym(RTLD_NEXT, "__libc_start_main");
-    return original_libc_start_main(main, argc, argv, init, fini, rtld_fini, stack_end);
+    orig_libc_start_main = dlsym(RTLD_NEXT, "__libc_start_main");
+    return orig_libc_start_main(main, argc, argv, init, fini, rtld_fini, stack_end);
 }
 
-void _exit(int status) {
-    void (*original_exit)(int);
-    original_exit = dlsym(RTLD_NEXT, "_exit");
+void exit(int status) {
+    void (*orig_exit)(int) __attribute__ ((noreturn));
+    //fprintf(stderr, "PRELOAD: _exit\n");
+    orig_exit = dlsym(RTLD_NEXT, "exit");
     close(bs.fd);
     lkl_sys_halt();
-    original_exit(status);
+    orig_exit(status);
 }
 
 int open(const char *subpath, int flags, ...) {
     char path[PATH_MAX];
-
+    fprintf(stderr, "PRELOAD: open\n");
     snprintf(path, sizeof(path), "%s/%s", mpoint, subpath);
-
+    //fprintf(stderr, "PRELOAD: open %s %s\n", subpath, path);
     return lkl_sys_open(path, LKL_O_RDONLY, 0);
 }
 
 ssize_t read(int fd, void *buf, size_t count) {
+    //fprintf(stderr, "PRELOAD: read\n");
     return lkl_sys_read(fd, buf, count);
+}
+
+int fstat(int fd, struct stat *buf) {
+    //fprintf(stderr, "PRELOAD: fstat\n");
+    return 0;
 }
