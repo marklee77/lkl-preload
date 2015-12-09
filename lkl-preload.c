9506d9abe8d62d5113ba1f49541f2843d8256bba
Mark Stillwell <mark@stillwell.me>
2015-12-09 08:16:16 +0000

    note...

--- lkl-preload.c
+++ lkl-preload.c
@@ -87,6 +87,8 @@ int __libc_start_main(int (*main)(int,char **,char **), int argc, char **argv,
                                 fini, rtld_fini, stack_end);
 }
 
+
+// FIXME: wrapper for fini?
 void exit(int status) {
     void (*orig_exit)(int) __attribute__((noreturn));
     int (*orig_close)(int);
@@ -98,6 +100,7 @@ void exit(int status) {
     lkl_sys_halt();
     orig_exit(status);
 }
+
 
 int open(const char *path, int flags, ...) {
     char fullpath[PATH_MAX];
@@ -143,10 +146,12 @@ int open(const char *path, int flags, ...) {
     return lkl_sys_open(fullpath, lkl_flags, mode);
 }
 
+
 int close(int fd) {
     fprintf(stderr, "PRELOAD: close\n");
     return lkl_sys_close(fd);
 }
+
 
 int __fxstat64(int ver, int fd, struct stat64 *buf) {
     struct lkl_stat lkl_stat;
@@ -172,10 +177,12 @@ int __fxstat64(int ver, int fd, struct stat64 *buf) {
     return ret;
 }
 
+
 ssize_t read(int fd, void *buf, size_t count) {
     fprintf(stderr, "PRELOAD: read\n");
     return lkl_sys_read(fd, buf, count);
 }
+
 
 ssize_t pread64(int fd, void *buf, size_t count, off_t offset) {
     fprintf(stderr, "PRELOAD: pread64 %d, %zd, %zd\n", fd, count, offset);    
@@ -193,7 +200,6 @@ ssize_t pread64(int fd, void *buf, size_t count, off_t offset) {
 ssize_t pwrite64(int fd, const void *buf, size_t count, off_t offset) {
     ssize_t ret = lkl_sys_pwrite64(fd, buf, count, offset);
 
-    // FIXME: is this documented anywhere?
     if (0 > ret) {
         errno = -ret;
         ret = -1;
